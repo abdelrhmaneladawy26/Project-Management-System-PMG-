639a9802a04588c7ea91ea702eacbf14870c3e42
abdelrhmaneladawy26 <abdelrhmaneladawy26@gmail.com>
2023-12-21 07:58:59 +0200

    created layout routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "@fortawesome/fontawesome-free/css/all.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// import { ToastContainer } from "react-toastify";
+import "./App.css";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Login from "./Components/Login/Login";
+import Projects from "./Components/Projects/Projects";
+import Register from "./Components/Register/Register";
+import RequestReset from "./Components/RequestReset/RequestReset";
+import ResetPassword from "./Components/ResetPassword/ResetPassword";
+import Tasks from "./Components/Tasks/Tasks";
+import Users from "./Components/Users/Users";
+import VerifyUser from "./Components/VerifyUser/VerifyUser";
+import AuthLayout from "./Shared/AuthLayout/AuthLayout";
+import MasterLayout from "./Shared/MasterLayout/MasterLayout";
+import NotFound from "./Shared/NotFound/NotFound";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <AuthLayout />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Login /> },
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+      { path: "request-reset", element: <RequestReset /> },
+      { path: "reset-password", element: <ResetPassword /> },
+      { path: "verify-user", element: <VerifyUser /> },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <MasterLayout />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Dashboard /> },
+      { path: "tasks", element: <Tasks /> },
+      { path: "projects", element: <Projects /> },
+      { path: "users", element: <Users /> },
+    ],
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={routes} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Shared/MasterLayout/MasterLayout.tsx
+++ src/Shared/MasterLayout/MasterLayout.tsx
@@ -1,3 +1,23 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../NavBar/NavBar";
+import SideBar from "../SideBar/SideBar";
+import Header from "../Header/Header";
+
 export default function MasterLayout() {
-  return <div>MasterLayout</div>;
+  return (
+    <div>
+      <div className="container-fluid">
+        <NavBar />
+        <div className="row">
+          <div className="col-md-3 bg-danger">
+            <SideBar />
+          </div>
+          <div className="col-md-9">
+            <Header />
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
