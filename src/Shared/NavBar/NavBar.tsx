778b8084e861b95db3cb9f3cb58e00d8992716cd
abdelrhmaneladawy26 <abdelrhmaneladawy26@gmail.com>
2023-12-24 11:42:08 +0200

    Created Navbar

--- src/Shared/NavBar/NavBar.tsx
+++ src/Shared/NavBar/NavBar.tsx
@@ -1,3 +1,27 @@
+import { useContext } from "react";
+import Logo from "../../assets/images/logo.png";
+import { AuthContext } from "../../Context/AuthContext";
 export default function NavBar() {
-  return <div>NavBar</div>;
+  const { userData } = useContext(AuthContext);
+  console.log(userData);
+  return (
+    <div>
+      <nav className="d-flex bg-white">
+        <div className="container-fluid">
+          <a className="navbar-brand">
+            <img src={Logo} alt="" />
+          </a>
+        </div>
+        <div className="navUserData d-flex align-items-center me-5 ">
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <h6>{userData?.userName}</h6>
+            <span className="">{userData?.userEmail}</span>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
